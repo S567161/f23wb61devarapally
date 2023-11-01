1b0f8f3de2c5757af78ffb8b2770946c887c9797
S567161 <142834279+S567161@users.noreply.github.com>
2023-10-31 23:33:43 -0500

    added wolf

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var WolfRouter = require('./routes/Wolf');
+
 
 var app = express();
 
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/Wolf', WolfRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
