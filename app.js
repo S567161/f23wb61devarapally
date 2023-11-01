518056ec30691bd3d71d8103449ef293b4c3b893
S567161 <142834279+S567161@users.noreply.github.com>
2023-10-31 23:50:21 -0500

    added images code

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var WolfRouter = require('./routes/Wolf');
 var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 
 
 var app = express();
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Wolf', WolfRouter);
 app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
